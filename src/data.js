a68d8617a02092da17a530cdfbf1444e79a73a13
Faiz U. Shaikh <61307003+codealive-fs@users.noreply.github.com>
2021-12-05 12:13:18 +0500

    nodejs project first commit

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const products = [
+  {
+    id: 1,
+    img: "https://images.unsplash.com/photo-1506038634487-60a69ae4b7b1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=766&q=80",
+    link: "http://lama.dev",
+  },
+  {
+    id: 2,
+    img: "https://i.ibb.co/189fxb7/hans-isaacson-l1-QTGpdkqb-A-unsplash.jpg",
+    link: "http://lama.dev",
+  },
+  {
+    id: 3,
+    img: "https://images.unsplash.com/photo-1509514026798-53d40bf1aa09?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80",
+    link: "http://lama.dev",
+  },
+  {
+    id: 4,
+    img: "https://images.unsplash.com/photo-1591280063444-d3c514eb6e13?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
+    link: "http://lama.dev",
+  },
+  {
+    id: 5,
+    img: "https://images.unsplash.com/photo-1517984922331-8dbaa8ffa9c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80",
+    link: "http://lama.dev",
+  },
+  {
+    id: 6,
+    img: "https://images.unsplash.com/photo-1507608616759-54f48f0af0ee?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    link: "http://lama.dev",
+  },
+];
